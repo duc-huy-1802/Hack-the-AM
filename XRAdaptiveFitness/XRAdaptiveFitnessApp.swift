e02adf29c1a8ce0cfb0b343d1aa8f00363560774
Duc Huy Nguyen <duchuynguyen1802@gmail.com>
2026-04-18 14:28:22 -0700

    Initial Commit

---
+++ XRAdaptiveFitness/XRAdaptiveFitnessApp.swift
@@ -0,0 +1,18 @@
+//
+//  XRAdaptiveFitnessApp.swift
+//  XRAdaptiveFitness
+//
+//  Created by iguest on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct XRAdaptiveFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .windowStyle(.volumetric)
+    }
+}
